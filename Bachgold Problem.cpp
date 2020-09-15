d3b3029d0fea66fa09a2d2e507919b45f898e4b5
Sanidhya Pawar <37259966+pawarss1@users.noreply.github.com>
2020-09-16 01:58:11 +0530

    Update Bachgold Problem.cpp

--- Bachgold Problem.cpp
+++ Bachgold Problem.cpp
@@ -9,13 +9,13 @@ int main()
     if(n % 2 == 0){
     	cout << n/2 <<endl;
     	for(int i = 1; i <= (n/2); i++){
-    		cout << 2<<" ";
+    		cout << 2 <<" ";
 		}
 	}
 	else{
 		cout << n/2 << endl;
 		for(int i = 1; i <= ((n/2)-1); i++){
-			cout << 2<<" ";
+			cout << 2 <<" ";
 		}
 		cout << 3;
 	}
